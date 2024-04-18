0f7a37ddec8dc858d1f45671842da43cd3ec2993
Norazhous <SjieZHOU@outlook.com>
2024-04-18 17:50:39 +0100

    line plotting update

--- dataAnalysis/src/components/CanvasDrawing.vue
+++ dataAnalysis/src/components/CanvasDrawing.vue
@@ -1,14 +1,103 @@
 <template>
-    <div class="image-container">
+    <div class="border-dashed">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2> p-h Chart for Enthalpy Value (Student)</h2>
+               
+                <div id="parent" class="image-container">
 
-        <!-- <img src="../assets/R134_p_h_diagram.svg" alt="Background" @load="adjustCanvasSize"> -->
-        <!-- <img src="/images/R134_p_h_diagram.svg" alt="Background"> -->
-        <canvas ref="canvas" @mousedown="startDrawing" @mouseup="endDrawing" @mousemove="drawLine"
-            @mouseout="endDrawing" width="550" height="400"></canvas>
-        <div>
-            <button type="button" class="btn btn-primary" @click="clearLine">Clear</button>
+                    <!-- <img src="../assets/R134_p_h_diagram.svg" alt="Background" @load="adjustCanvasSize"> -->
+                    <!-- <img src="/images/R134_p_h_diagram.svg" alt="Background"> -->
+                    <canvas ref="canvas" @mousedown="startDrawing" @mouseup="endDrawing" @mousemove="drawLine"
+                        @mouseout="endDrawing"></canvas>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <button type="button" class="col-12 btn btn-primary" @click="clearLine">Plotting line </button>
+                    </div>
+                    <div class="col-lg-3">
+                        <button type="button" class="col-12 btn btn-primary" @click="clearLine">Straight line </button>
+                    </div>
+                    <div class="col-lg-3">
+                        <button type="button" class="col-12 btn btn-primary" @click="clearLine">Clear</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h2> Calculation Results (Student) </h2>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="table-light">
+                            <tr>
+                                <th colspan="5">Enthalpy Value</th>
+                            </tr>
+                            <tr>
+
+                                <th>H1(KJ/Kg)</th>
+                                <th>H2(KJ/Kg)</th>
+                                <th>H3(KJ/Kg)</th>
+                                <th>H4(KJ/Kg)</th>
+                                <th>H5(KJ/Kg)</th>
+
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+                                <td><input type="number" name="" class="inputnumber" id="H1" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="H2" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="H3" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="H4" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="H5" placeholder="22"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table">
+                        <thead class="table-light">
+                            <tr>
+                                <th colspan="3">Parameters Calculation</th>
+                            </tr>
+                            <tr>
+                                <th>Mass flowrate m(Kg/s)</th>
+                                <th>Refrigeration capacity QL(KW)</th>
+                                <th>Condensation capacity QH(KW)</th>
+                                <!-- <th>Compressor work</th>
+                            <th>Coefficient of performance </th>
+                            <th>Compressor compression ratio</th> -->
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+
+                                <td><input type="number" name="" class="inputnumber" id="m" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="QL" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="QH" placeholder="22"></td>
+                                <!-- <td><input type="number" name="" class="inputnumber" id="F" placeholder="22"></td>
+                            <td><input type="number" name="" class="inputnumber" id="E" placeholder="22"></td>
+                            <td><input type="number" name="" class="inputnumber" id="E" placeholder="22"></td> -->
+
+                            </tr>
+                        </tbody>
+                        <thead class="table-light">
+                            <tr>
+                                <th>Compressor work W(KW)</th>
+                                <th>Coefficient of performance COP</th>
+                                <th>Compressor compression ratio η</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <tr>
+                                <td><input type="number" name="" class="inputnumber" id="W" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="COP" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="n" placeholder="22"></td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
+
 
 </template>
 
@@ -27,20 +116,47 @@ export default {
     },
     mounted() {
         // adjust the size of the canvas
-        this.$nextTick(() => {
-            const canvas = this.$refs.canvas;
-            canvas.width = 550;
-            canvas.height = 400;
-        });
+        // this.$nextTick(() => {
+        //     const canvas = this.$refs.canvas;
+        //     const parent = document.getElementById('parent')
+        //     // canvas.width = window.innerWidth;
+        //     // canvas.height = window.innerHeight;
+        //     // canvas.style.width = parent.height;
+        //     // canvas.style.height = parent.height;
+        //     // canvas.width = parent.offsetWidth;
+        //     // canvas.height = parent.offsetHeight;
+        //     // canvas.style.width = '100%';
+        //     // canvas.style.height = '100%';
+        //     canvas.width = canvas.offsetWidth;
+        //     canvas.height = canvas.offsetHeight;
+        // });
+        this.setupCanvas();
+        // window.addEventListener('resize', this.setupCanvas);
     },
+    // beforeDestroy() {
+    //     window.removeEventListener('resize', this.setupCanvas); // Clean up the event listener
+    // },
     methods: {
+        setupCanvas() {
+            const canvas = this.$refs.canvas;
+            // const parent = document.getElementById('parent');
+            // canvas.width = parent.offsetWidth;
+            // canvas.height = parent.offsetHeight;
+            canvas.width = canvas.offsetWidth;
+            canvas.height = canvas.offsetHeight;
+        },
+
         // drawing lines function
         startDrawing(e) {
             this.drawing = true;
             const rect = this.$refs.canvas.getBoundingClientRect();
+            console.log(rect);
+            // e.clientX = rect.left;
+            // e.clientY = rect.top;
             this.startX = e.clientX - rect.left;
             this.startY = e.clientY - rect.top;
             this.drawLine(e);
+            console.log(e);
         },
         endDrawing() {
             this.drawing = false;
@@ -69,12 +185,17 @@ export default {
             console.log("clear")
         }
     },
+    //https://stackoverflow.com/questions/49885020/drawing-a-straight-line-using-mouse-events-on-canvas-in-javascript
 };
 </script>
 
 <style>
 .image-container {
-    display: inline;
+    display: inline-block;
+    width: 100%;
+    height: 500px;
+    position: relative;
+
 }
 
 .image-container canvas {
@@ -82,5 +203,9 @@ export default {
     background-size: 100% 100%;
     top: 0;
     left: 0;
+    width: 100%;
+    height: 95%;
+    position: absolute;
+
 }
 </style>
--- dataAnalysis/src/components/MeasuredDataFeedback.vue
+++ dataAnalysis/src/components/MeasuredDataFeedback.vue
@@ -22,12 +22,12 @@
                         </thead>
                         <tbody class="table-group-divider">
                             <tr>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="time" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="T1" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="T2" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="T3" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="T4" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="T5" placeholder="22"></td>
                                 <!-- <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
                                 <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
                                 <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
@@ -57,12 +57,12 @@
                                 <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
                                 <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
                                 <td><input type="number" name="" id="inputnumber" placeholder="22"></td> -->
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
-                                <td><input type="number" name="" id="inputnumber" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="P1" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="P2" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="P3" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="F" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="E" placeholder="22"></td>
+                                <td><input type="number" name="" class="inputnumber" id="ASP" placeholder="22"></td>
                             </tr>
                         </tbody>
                     </table>
@@ -76,7 +76,7 @@
                         <!-- <button type="button" class="btn btn-primary">Reset</button> -->
                     </div>
                     <div class="col-3">
-                        <button type="button" class="btn btn-primary">Reset</button>
+                        <button type="button" class="btn btn-primary" @click="reset()">Reset</button>
                     </div>
                     <div class="col-3">
 
@@ -85,7 +85,7 @@
 
             </div>
             <div class="col-lg-6">
-                <h2>Measured Data Evaluation</h2>
+                <h2>Measured Data Feedback</h2>
                 <div>
                     <canvas id="MeasuredDataChart" width="450" height="200"></canvas>
                 </div>
@@ -104,8 +104,6 @@ import 'chartjs-plugin-streaming';
 // import 'chartjs-adapter-luxon';
 // import 'luxon';
 
-
-
 export default {
     name: "MeasuredDataFeedback",
     props: {
@@ -116,6 +114,7 @@ export default {
         return {
             DataQualityDataset: [],
             updateFlag: false,
+            inputTime: 0, 
         }
     },
     mounted() {
@@ -131,7 +130,6 @@ export default {
                     y: 1 - Math.exp(-i * 0.1)
                 })
             };
-            console.log(this.DataQualityDataset);
 
         },
 
@@ -145,12 +143,13 @@ export default {
                 data: {
                     labels: [], // Fixed x-axis labels
                     datasets: [{
-                        label: 'Data Quality',
+                        label: 'Data Quality Curve',
                         backgroundColor: 'rgb(255, 0, 0)',
                         borderColor: 'rgb(255, 0, 0)',
                         borderWidth: 1,
                         data: [],  // Initialize with empty data
                         pointRadius: 0,
+                        // hidden: () => this.updateAction(),
                     },
 
                     {
@@ -158,7 +157,9 @@ export default {
                         backgroundColor: 'rgb(0, 0, 255)',
                         borderColor: 'rgb(0, 0, 255)',
                         borderWidth: 1,
-                        data: []  // Initialize with empty data
+                        data: [],
+                        pointRadius: 8,
+                        pointStyle: 'rectRot',
                     },],
                 },
                 options: {
@@ -168,7 +169,7 @@ export default {
                             type: 'linear', // Fixed x-axis scale
                             position: 'bottom', // Position x-axis at the bottom
                             ticks: {
-                                stepSize: 10, // Step size for x-axis
+                                stepSize: 5, // Step size for x-axis
                                 min: 0,      // Minimum value for x-axis
                                 max: 500,      // Maximum value for x-axis
                             },
@@ -187,7 +188,7 @@ export default {
                             },
                             title: {
                                 display: true,
-                                text: 'Quality'
+                                text: 'Data Quality'
                             }
                         }
                     },
@@ -196,18 +197,19 @@ export default {
                             annotations: {
                                 box1: {
                                     type: 'box',
-                                    xMin: 18,
-                                    xMax: 35,
+                                    xMin: 20,
+                                    xMax: 45,
                                     yMin: 0,
-                                    yMax: 1,
+                                    yMax: 1.6,
                                     backgroundColor: 'rgba(255,165,0, 0.25)',
+                                    borderColor: 'rgba(255,165,0, 0.25)',
                                     display: () => this.updateAction(),
                                     drawTime: 'afterDraw',
                                 },
                                 label1: {
                                     display: () => this.updateAction(),
                                     type: 'label',
-                                    xValue: 26,
+                                    xValue: 33,
                                     yValue: 0.3,
                                     backgroundColor: 'rgba(245,245,245)',
                                     content: ['Best Range'],
@@ -222,30 +224,67 @@ export default {
                 },
             });
         },
-        
+
         //update function for dispaly
-        updateAction(){
-            return this.updateFlag ;
+        updateAction() {
+            return this.updateFlag;
         },
 
 
         updateChart() {
-            this.updateFlag=true;
-            this.QualityDataset();
-            this.MeasuredDataChart.data.datasets[0].data = this.DataQualityDataset;
-            this.MeasuredDataChart.update();
-            console.log(this.MeasuredDataChart.data.datasets[0].data);
-            console.log(this.MeasuredDataChart.options.plugins.annotation.annotation.box1.display)
+            this.inputTime = document.getElementById('time').value;
+            if (this.MeasuredDataChart.data.datasets[0].data.length == 0 && this.inputTime != ''&& this.DataQualityDataset.length ==0) {
+                //update the quality line in the chart and show the best range annotation
+                this.updateFlag = true;
+                this.QualityDataset();
+                this.MeasuredDataChart.data.datasets[0].data = this.DataQualityDataset;
 
-        }
+                //update the data input
+                // let inputTime = document.getElementById('time').value;
+                let qualityCalculation = 1 - Math.exp(-this.inputTime * 0.1);
+                this.MeasuredDataChart.data.datasets[1].data.push({
+                    x: this.inputTime,
+                    y: qualityCalculation,
+                }),
+                this.MeasuredDataChart.update();
+
+            } else if (this.inputTime == '') {
+                alert("Please input measured data!")
+
+            }
+            else {
+                this.updateFlag = true;
+                this.MeasuredDataChart.data.datasets[0].data = this.DataQualityDataset;
+                //update the data input 
+                this.inputTime = document.getElementById('time').value;
+                let qualityCalculation = 1 - Math.exp(-this.inputTime * 0.1);
+                this.MeasuredDataChart.data.datasets[1].data = [];
+                this.MeasuredDataChart.data.datasets[1].data.push({
+                    x: this.inputTime,
+                    y: qualityCalculation,
+                }),
+                this.MeasuredDataChart.update();
+            }
+
+        },
+
+        //reset the data in the input form and feedback chart
+        reset() {
+            this.updateFlag = false;
+            this.MeasuredDataChart.data.datasets[0].data = [];
+            this.MeasuredDataChart.data.datasets[1].data = [];
+            this.MeasuredDataChart.update();
+        },
 
 
     },
+
+
 };
 </script>
 
 <style>
-#inputnumber {
+.inputnumber {
     width: 70px;
 }
 </style>
